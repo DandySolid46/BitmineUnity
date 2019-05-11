7699b260ff8b930c0819b5bd02293e0b5a9814bd
Lucas Liduino <lucasliduino@gmail.com>
2019-05-11 13:11:04 -0300

    Cores

--- Assets/scriptus/Player.cs
+++ Assets/scriptus/Player.cs
@@ -1,5 +1,31 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
     public PlayerData data;
+    public BoardData boardData;
+    Renderer renderer;
+    void Awake() {
+        renderer = GetComponentInChildren<MeshRenderer>();
+    }
+    void Start() {
+        renderer.material.color = GetColorFromType(data.cellType);
+    }
+    public static Color GetColorFromType(Board.Cell cellType){
+        switch (cellType){
+            case Board.Cell.PlayerBlue:
+                return Color.blue;
+            case Board.Cell.PlayerRed:
+                return Color.red;
+            case Board.Cell.PlayerPurple:
+                return Color.magenta;
+            case Board.Cell.PlayerYellow:
+                return Color.yellow;
+            default:
+                throw new Exception("no color for " + cellType);
+        }
+    }
+    void Update(){
+        transform.position = boardData.grid.GetCellCenterLocal((Vector3Int)data.position);
+    }
 }
