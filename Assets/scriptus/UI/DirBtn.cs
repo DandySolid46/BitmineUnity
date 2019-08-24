c047163bfe0c09a41ca2b3562d28aed17d935713
Lucas Liduino <lucasliduino@gmail.com>
2019-08-24 12:37:42 -0300

    btn some no 5stg

--- Assets/scriptus/UI/DirBtn.cs
+++ Assets/scriptus/UI/DirBtn.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class DirBtn : MonoBehaviour{
+public class DirBtn : MonoBehaviour {
     public enum Direction {
         Up = 0,
         Right = 1,
         Down = 2,
         Left = 3,
+        Nill = 4,
     }
 
     static readonly float[] DirectionAngles = new float[]{
@@ -13,6 +15,11 @@ public class DirBtn : MonoBehaviour{
     };
 
     public Direction direction = Direction.Up;
+    Graphic graphic;
+
+    void Awake (){
+        graphic = GetComponent<Graphic>();
+    }
 
     void Start(){
         ApplyDirection();
@@ -20,14 +27,19 @@ public class DirBtn : MonoBehaviour{
 
     public void OnClick(){
         int dir = (int)direction;
-        dir = (dir + 1) % 4;
+        dir = (dir + 1) % 5;
         direction = (Direction)dir;
         ApplyDirection();
     }
-   void ApplyDirection() {
-       var rot = transform.localRotation.eulerAngles;
-       rot.z = DirectionAngles[(int)direction];
-       transform.localRotation = Quaternion.Euler(rot);
+    void ApplyDirection() {
+        if (direction == Direction.Nill) {
+            graphic.color = Color.clear;
+        } else {
+            graphic.color = Color.white;
+            var rot = transform.localRotation.eulerAngles;
+            rot.z = DirectionAngles[(int)direction];
+            transform.localRotation = Quaternion.Euler(rot);
+        }
     }
     public Direction GetDir() => direction;
     public void SetDir(Direction dir){
